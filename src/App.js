4d27f7631f5e8a0ed57afe82f14e0fbdb2976267
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-08-19 19:21:05 +0200

    Add: initail setup for route V-6

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import Directory from './components/Directory/Directory';
-
-import hats from './images/hats.png';
-import jackets from './images/jackets.png';
-import mens from './images/men.png';
-import sneakers from './images/sneakers.png';
-import womens from './images/womens.png';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'hats',
-			imageUrl: hats,
-		},
-		{
-			id: 2,
-			title: 'jackets',
-			imageUrl: jackets,
-		},
-		{
-			id: 3,
-			title: 'sneakers',
-			imageUrl: sneakers,
-		},
-		{
-			id: 4,
-			title: 'womens',
-			imageUrl: womens,
-		},
-		{
-			id: 5,
-			title: 'mens',
-			imageUrl: mens,
-		},
-	];
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+		</Routes>
+	);
 };
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,40 @@
+import Directory from '../../components/Directory/Directory';
+
+import hats from '../../images/hats.png';
+import jackets from '../../images/jackets.png';
+import mens from '../../images/men.png';
+import sneakers from '../../images/sneakers.png';
+import womens from '../../images/womens.png';
+
+const Home = () => {
+	const categories = [
+		{
+			id: 1,
+			title: 'hats',
+			imageUrl: hats,
+		},
+		{
+			id: 2,
+			title: 'jackets',
+			imageUrl: jackets,
+		},
+		{
+			id: 3,
+			title: 'sneakers',
+			imageUrl: sneakers,
+		},
+		{
+			id: 4,
+			title: 'womens',
+			imageUrl: womens,
+		},
+		{
+			id: 5,
+			title: 'mens',
+			imageUrl: mens,
+		},
+	];
+	return <Directory categories={categories} />;
+};
+
+export default Home;
