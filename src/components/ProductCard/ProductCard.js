3ca0462257b66022a44be33508b4138c86981b95
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-08-24 06:10:59 +0200

    renaming product cart classes

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 import Button from '../Button/Button';
 import './ProductCard.scss';
 
 const ProductCard = ({ product }) => {
 	const { name, price, imageUrl } = product;
+	const { addItemToCart } = useContext(CartContext);
 	return (
 		<div className='product-card'>
 			<img src={imageUrl} alt={`${name}`} />
-			<div className='footer'>
-				<span className='name'>{name}</span>
-				<span className='price'>{price}</span>
+			<div className='product-card__footer'>
+				<span className='product-card__name'>{name}</span>
+				<span className='product-card__price'>{price}</span>
 			</div>
-			<Button buttonType='inverted'>Add to card</Button>
+			<Button onClick={() => addItemToCart(product)} buttonType='inverted'>
+				Add to card
+			</Button>
 		</div>
 	);
 };
