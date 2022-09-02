740c5157b9921bedd4e98f3979dffd120fc3e51d
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-09-02 13:55:17 +0200

    updating categories and cart to use reducer

--- src/contexts/CategoriesContext.js
+++ src/contexts/CategoriesContext.js
@@ -1,18 +1,44 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useReducer, useState } from 'react';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase';
+import { createAction } from '../utils/reducer/reducer.utils';
+
+const CATEGORIES_ACTION_TYPES = {
+	SET_CATEGORIES_MAP: 'SET_CATEGORIES_MAP',
+};
+
+const INITIAL_STATE = {
+	categoriesMap: {},
+};
+
+const categoriesReducer = (state, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+			return { ...state, categoriesMap: payload };
+
+		default:
+			throw new Error(`Unhandled type ${type} in categoriesReducer`);
+	}
+};
 
 const CategoriesContext = createContext({
 	categoriesMap: {},
 });
 
 const CategoriesProvider = ({ children }) => {
-	const [categoriesMap, setCategoriesMap] = useState({});
+	const [{ categoriesMap }, dispatch] = useReducer(
+		categoriesReducer,
+		INITIAL_STATE
+	);
 	const value = { categoriesMap };
 
 	useEffect(() => {
 		const getCategoriesMap = async () => {
 			const categoryMap = await getCategoriesAndDocuments();
-			setCategoriesMap(categoryMap);
+			dispatch(
+				createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoryMap)
+			);
 		};
 
 		getCategoriesMap();
