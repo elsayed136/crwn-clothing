fef47122d7cecfadb2eb85f2fecf3cf8c40a4604
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-09-11 21:10:28 +0200

    using isLoading to render spinner

--- src/pages/CategoriesPreview/CategoriesPreview.js
+++ src/pages/CategoriesPreview/CategoriesPreview.js
@@ -1,18 +1,27 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import CategoryPreview from '../../components/CategoryPreview/CategoryPreview';
-import { selectCategoriesMap } from '../../store/categories/categoriesSelector';
+import Spinner from '../../components/Spinner/Spinner';
+import {
+	selectCategoriesMap,
+	selectIsLoading,
+} from '../../store/categories/categoriesSelector';
 
 const CategoriesPreview = () => {
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectIsLoading);
 	return (
 		<Fragment>
-			{Object.keys(categoriesMap).map(title => {
-				const products = categoriesMap[title];
-				return (
-					<CategoryPreview key={title} title={title} products={products} />
-				);
-			})}
+			{isLoading ? (
+				<Spinner />
+			) : (
+				Object.keys(categoriesMap).map(title => {
+					const products = categoriesMap[title];
+					return (
+						<CategoryPreview key={title} title={title} products={products} />
+					);
+				})
+			)}
 		</Fragment>
 	);
 };
--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -2,13 +2,18 @@ import { Fragment, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/ProductCard/ProductCard';
-import { selectCategoriesMap } from '../../store/categories/categoriesSelector';
+import Spinner from '../../components/Spinner/Spinner';
+import {
+	selectCategoriesMap,
+	selectIsLoading,
+} from '../../store/categories/categoriesSelector';
 import { CategoryContainer, Title } from './Category.styles';
 
 const Category = () => {
 	const { category } = useParams();
 	const categoriesMap = useSelector(selectCategoriesMap);
 	const [products, setProducts] = useState(categoriesMap[category]);
+	const isLoading = useSelector(selectIsLoading);
 
 	useEffect(() => {
 		setProducts(categoriesMap[category]);
@@ -17,12 +22,16 @@ const Category = () => {
 	return (
 		<Fragment>
 			<Title>{category.toUpperCase()}</Title>
-			<CategoryContainer>
-				{products &&
-					products.map(product => {
-						return <ProductCard key={product.id} product={product} />;
-					})}
-			</CategoryContainer>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<CategoryContainer>
+					{products &&
+						products.map(product => {
+							return <ProductCard key={product.id} product={product} />;
+						})}
+				</CategoryContainer>
+			)}
 		</Fragment>
 	);
 };
